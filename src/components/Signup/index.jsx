2f878cad6aba23b640a3476054446e14cab64444
Mohamed Ansary <85570638+ansary29@users.noreply.github.com>
2023-07-25 21:50:21 +0530

    Update index.jsx

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "https://mernloginsignup.herokuapp.com/api/users";
+			const url = "https://loginsignup-fnxp.onrender.com/api/users";
 			const { data: res } = await axios.post(url, data);
 			navigate("/login");
 			console.log(res.message);
